2e9e5b0392cac0ee28d79fd1b16511c4fe17e2b6
Ross-0 <stefanomotta933@gmail.com>
2026-05-09 19:54:46 +0200

    Calcolatore funzionante

--- calcolatore/views.py
+++ calcolatore/views.py
@@ -28,7 +28,7 @@ def calculate(request):
     if csv_data.isnull().values.any():
         # Opzionale: puoi identificare dove sono gli errori
         colonne_con_nan = csv_data.columns[csv_data.isnull().any()].tolist()
-        return HttpResponse(status=422, content=f"Errore: CSV doesn't have value in this columns: {colonne_con_nan}")
+        return HttpResponse(status=422, content=f"Errore: CSV doesn'csv_data[time_column] have value in this columns: {colonne_con_nan}")
         # 2. Controlla che tutti i valori siano maggiori di 0
         # (Assumendo che siano tutte colonne numeriche)
 
@@ -49,40 +49,65 @@ def calculate(request):
             voltage_column = csv_data.columns[0] if i == 1 else csv_data.columns[1]
 
     if counter == 2:
-        return HttpResponse(status=422, content=f"Errore: CSV doesn't have a correct time column")
+        return HttpResponse(status=422, content=f"Errore: CSV doesn'csv_data[time_column] have a correct time column")
 
     if not _is_step(csv_data, time_column):
-        return HttpResponse(status=422, content=f"Errore: time column isn't in constant step")
+        return HttpResponse(status=422, content=f"Errore: time column isn'csv_data[time_column] in constant step")
     time_step = csv_data[time_column][1]
 
     if csv_data[voltage_column][len(csv_data[voltage_column]) - 1] != 0:
-        return HttpResponse(status=422, content=f"Errore: voltage column last value isn't 0")
+        return HttpResponse(status=422, content=f"Errore: voltage column last value isn'csv_data[time_column] 0")
 
     curve = _get_curve(csv_data, voltage_column, time_column)
-    rects, result = _rectangle_method(csv_data, time_step, voltage_column, time_column)
+    rects, rects_result = _rectangle_method(csv_data, time_step, voltage_column, time_column)
+    trapezius, trapezius_result = _trapezius_method(csv_data, time_step, voltage_column, time_column)
+    simpson, simpson_result = _simpson_method(csv_data, time_step, voltage_column, time_column)
+
     context = {'curve': json.dumps(curve),
-               'result': result,
-               'rects': json.dumps(rects),}
+               'rects_result': rects_result,
+               'rects': json.dumps(rects),
+               'trapezius_result': trapezius_result,
+               'trapezius': json.dumps(trapezius),
+               'simpson_result': simpson_result,
+               'simpson': json.dumps(simpson),
+               'requested_method': request.POST.get('integral_type')
+               }
 
     return render(request, "calcolatore/chart.html", context)
 
 def _is_step(data_frame, time_column):
-    step_atteso = data_frame[time_column][1]
-    differenze = data_frame[time_column].diff().dropna()
+    step = data_frame[time_column][1]
+    difference = data_frame[time_column].diff().dropna()
 
     # 3. Controlliamo se tutte le differenze sono uguali allo step atteso
     # Usiamo un margine di errore minimo (tol) se lavoriamo con float,
     # per gli interi il confronto diretto == è perfetto.
-    confronto_bool = np.isclose(differenze, step_atteso)
-    valido = confronto_bool.all()
-    return valido
+    confronto_bool = np.isclose(difference, step)
+    valid = confronto_bool.all()
+    return valid
 
-def _get_curve(data_frame, voltage_column, time_column):
-    return [[data_frame[time_column][i], data_frame[voltage_column][i]] for i in range(len(data_frame[time_column]))]
+def _get_curve(csv_data, voltage_column, time_column):
+    return [[csv_data[time_column][i], csv_data[voltage_column][i]] for i in range(len(csv_data[time_column]))]
 
-def _rectangle_method(data_frame, time_step, voltage_column, time_column):
-    n = len(data_frame[time_column]) - 1
-    rects = [[data_frame[time_column][i], data_frame[time_column][i+1], data_frame[voltage_column][i]] for i in range(n)]
-    totale_voltaggio = data_frame[voltage_column].sum()
-    return rects, time_step * totale_voltaggio
+def _rectangle_method(csv_data, time_step, voltage_column, time_column):
+    n = len(csv_data[time_column]) - 1
+    rects = [[csv_data[time_column][i], csv_data[time_column][i + 1], csv_data[voltage_column][i]] for i in range(n)]
+    voltage_total = csv_data[voltage_column].sum()
+    return rects, time_step * voltage_total
 
+def _trapezius_method(csv_data, time_step, voltage_column, time_column):
+    n = len(csv_data[time_column]) - 1
+    traps = [[csv_data[time_column][i], csv_data[time_column][i + 1], csv_data[voltage_column][i], csv_data[voltage_column][i + 1]] for i in range(n)]
+    voltage_total = csv_data[voltage_column].sum()
+    return traps, time_step/2 * (2*voltage_total)
+
+def _simpson_method(csv_data, time_step, voltage_column, time_column):
+    n = len(csv_data[time_column]) - 1
+    simpson = [[csv_data[time_column][i], csv_data[time_column][i + 1], csv_data[time_column][i + 2], csv_data[voltage_column][i], csv_data[voltage_column][i + 1], csv_data[voltage_column][i + 2]] for i in range(0, n, 2)]
+    even_voltage_total = 0
+    for i in range(0, n, 2):
+        even_voltage_total += csv_data[voltage_column][i]
+    odd_voltage_total = 0
+    for i in range(1, n, 2):
+        odd_voltage_total += csv_data[voltage_column][i]
+    return simpson, time_step/3 * (2*even_voltage_total + 4*odd_voltage_total)
