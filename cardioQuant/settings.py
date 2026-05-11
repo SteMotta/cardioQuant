ad66d766f461ac3ec07c98ea84780eeaaf1837fc
Stefano Motta <103846397+SteMotta@users.noreply.github.com>
2026-05-11 12:46:10 +0200

    .

--- cardioQuant/settings.py
+++ cardioQuant/settings.py
@@ -133,6 +133,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'theme' / 'static',  # ← cartella corretta
+]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
